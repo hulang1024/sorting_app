282ec0ced204489f592bea8dd15d90b8980407ba
hulang <1013644379@qq.com>
2020-03-23 20:45:44 +0800

    Refactor: Rename LoginPage to Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sorting/pages/user/login.dart';
+import 'login.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: Login(),
     );
   }
 }
